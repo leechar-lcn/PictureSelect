301811010f0b69260793d5a5ea03acdc21a5b5eb
leechar-lcn <leechar@aliyun.com>
2020-05-07 11:53:31 +0800

    打包部署

---
+++ build/precache-manifest.33cc7206c2ebf8e9649a0479ed1da244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f193b365f6a27610627babc1c3d466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61bc97e48655df07de8",
+    "url": "/static/css/2.c0903d66.chunk.css"
+  },
+  {
+    "revision": "5af58fe2f3427f6fbcbe",
+    "url": "/static/css/main.ec030ee2.chunk.css"
+  },
+  {
+    "revision": "f61bc97e48655df07de8",
+    "url": "/static/js/2.bbb0a36e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bbb0a36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af58fe2f3427f6fbcbe",
+    "url": "/static/js/main.5fbdd125.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
